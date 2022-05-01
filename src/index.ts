1146baa3e41aac58dbf050a8f181e283002908a6
Sami Rachdi <rachdi.sami@gmail.com>
2022-05-01 11:27:57 +0000

    added PUG support

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
-app.listen(4000, () => {
-  console.log('server running on port 4000');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req:any, res:any) => {
+  res.render('index');
+})
+
+const server = app.listen(4000, () => {
+  console.log(`The application started on port :4000`);
 });
